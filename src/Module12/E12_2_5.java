d6490ae9570f3a15441c6f4247a446b0850c84d3
flussigkatz <flussigkatz@gmail.com>
2021-02-14 16:08:24 +0300

    Commit

--- src/Module12/E12_2_5.java
+++ src/Module12/E12_2_5.java
@@ -2,7 +2,7 @@ package Module12;
 
 public class E12_2_5 extends Thread {
 
-    @Override
+    /*@Override
     public void run() {
         try {
             System.out.println("Thread started");
@@ -10,8 +10,7 @@ public class E12_2_5 extends Thread {
             System.out.println("Thread interrupted");
         }
     }
-
-    public static void main(String[] args) {
+*/    public static void main(String[] args) {
         Thread thread = new E12_2_5();
         thread.start();
         thread.interrupt();
