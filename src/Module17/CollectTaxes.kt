268f2d45852a5da6dfefcea8a1d5b83bfc3593d9
flussigkatz <flussigkatz@gmail.com>
2021-03-23 14:27:12 +0300

    Commit

--- src/Module17/CollectTaxes.kt
+++
@@ -1,5 +0,0 @@
-package Module17
-
-interface CollectTaxes {
-    fun Collect()
-}
--- src/Module17/Kingdom.kt
+++ src/Module17/Kingdom.kt
@@ -1,27 +1,25 @@
 package module17
 
-import Module17.CollectTaxes
 import Module17.Peasant
 
 
 fun main() {
     val kingdom = Kingdom()
-    println(kingdom.ruler)
-//    println(kingdom.heirList)
-//    println(kingdom.archerList)
-//    println(kingdom.warriorList)
-    println(kingdom.peasantList)
-    println(kingdom.peasantList.size)
+    kingdom.taxCollector.collect(kingdom.peasantList)
     Ruler.rulerAnnouncement()
+    println(Kingdom.treasury)
 }
 
 class Kingdom {
-    var treasury = 0
+    companion object {
+        var treasury = 0
+    }
     var ruler = Ruler("Ivan")
     var heirList = mutableListOf<Heir>()
     val archerList = mutableListOf<Archer>()
     val warriorList = mutableListOf<Warrior>()
     val peasantList = mutableListOf<Peasant>()
+    val taxCollector = TaxCollector()
     fun bornHeir(name: String) {
         val heir = Heir(name)
         heirList.add(heir)
@@ -47,13 +45,13 @@ class Kingdom {
             when {
                 i % 2 == 0 -> peasantList.add(Peasant(Peasant.Occupation.BILDER))
                 i % 3 == 0 -> peasantList.add(Peasant(Peasant.Occupation.FARMER))
-                else ->peasantList.add(Peasant(Peasant.Occupation.WORKER))
+                else -> peasantList.add(Peasant(Peasant.Occupation.WORKER))
             }
         }
     }
 }
 
-fun fortunate (fortValue: Int): Int {
+fun fortunate(fortValue: Int): Int {
     if ((1..20).random() > 10) {
         return fortValue + (1..20).random()
     } else {
@@ -64,19 +62,21 @@ fun fortunate (fortValue: Int): Int {
 open class Ruler(val name: String) {
     var power: Int = (50..100).random()
     var intellect: Int = (60..200).random()
+
     companion object {
-        fun rulerAnnouncement () {
+        fun rulerAnnouncement() {
             println("Его Высокое Благородие Король в здании.")
         }
     }
+
     override fun toString(): String {
         return "Ruler($name power=$power intellect=$intellect)"
     }
 
 }
 
-class Heir(name: String): Ruler(name){
-    init{
+class Heir(name: String) : Ruler(name) {
+    init {
         power = fortunate(power)
         intellect = fortunate(intellect)
     }
@@ -88,4 +88,17 @@ data class Archer(val weapon: String, val armor: String = "LightArmor")
 
 data class Warrior(val weapon: String, val armor: String = "HardArmor")
 
-abstract class TaxCollector
+class TaxCollector : CollectTaxes {
+    override fun collect(peasantList: MutableList<Peasant>) {
+        for (i in 0 until peasantList.size)
+        if (peasantList.get(i).occupation == Peasant.Occupation.FARMER) {
+            Kingdom.treasury += 3
+        } else if (peasantList.get(i).occupation == Peasant.Occupation.BILDER) {
+            Kingdom.treasury += 2
+        } else Kingdom.treasury += 1
+    }
+}
+
+interface CollectTaxes {
+    fun collect(peasantList: MutableList<Peasant>) {}
+}
